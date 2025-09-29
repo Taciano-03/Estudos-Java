ef2fff4dabc78437c4ee888e8a2ef9a69d7b2be9
Gleidson <gleidsonlimataciano921@gmail.com>
2025-09-28 21:52:35 -0300

    Primeiro_commit-em-java

---
+++ src/BemVindo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+
+public class BemVindo {
+    public static void main(String[] args) {
+        String nome; 
+        int idade; 
+        double altura;
+        Scanner entrada = new Scanner(System.in); 
+        
+        System.out.print("Digite seu nome: "); 
+        nome = entrada.nextLine(); // capturar o que o usuário digitar
+
+        System.out.print("Digite sua idade: ");
+        idade = entrada.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        altura = entrada.nextDouble();
+
+        System.out.println("Olá, " + nome + "! Sua idade é " + idade + " e sua altura é " + altura + ".");
+        
+        entrada.close(); // boa prática: fechar o Scanner
+    }
+}
